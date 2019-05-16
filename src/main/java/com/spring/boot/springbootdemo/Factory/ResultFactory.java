892b76fbc422cc18fa0c3a2386dbf096848ef7ef
lwang <1530775176@qq.com>
2019-05-16 18:01:57 +0800

    login by vue

---
+++ src/main/java/com/spring/boot/springbootdemo/Factory/ResultFactory.java
@@ -0,0 +1,22 @@
+package com.spring.boot.springbootdemo.Factory;
+
+import com.spring.boot.springbootdemo.domain.Result;
+import com.spring.boot.springbootdemo.domain.ResultCode;
+
+public class ResultFactory {
+    public static Result buildSuccessResult(Object data) {
+        return buidResult(ResultCode.SUCCESS, "成功", data);
+    }
+
+    public static Result buildFailResult(String message) {
+        return buidResult(ResultCode.FAIL, message, null);
+    }
+
+    public static Result buidResult(ResultCode resultCode, String message, Object data) {
+        return buidResult(resultCode.code, message, data);
+    }
+
+    public static Result buidResult(int resultCode, String message, Object data) {
+        return new Result(resultCode, message, data);
+    }
+}
